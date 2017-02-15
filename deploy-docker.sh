594eb48684d8c335068c47d526b10020455b3451
Yamila Moreno <yamila.ms@gmail.com>
2017-02-15 22:21:37 +0100

    Minor fix

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker-compose build -t python-es-web:1.0 -f Dockerfile .
 echo "... Updating python-es-web: Restart the service"
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up -d --no-deps
